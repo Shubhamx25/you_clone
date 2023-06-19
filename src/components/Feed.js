8040437a178bb0ba432bee4a0d08d19db4f6608e
Shubhamx25 <shubhamy725p@gmail.com>
2023-06-19 18:27:13 +0530

    Eod 19 june

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react"
 import { Box, Stack, Typography } from "@mui/material";
-
+import Sidebar from './Sidebar';
 
 const Feed = () => {
   return (
@@ -8,11 +8,17 @@ const Feed = () => {
         flexDirection: {sx: 'column', md: 'row'}
      }}>
         <Box sx={{ 
-            height : {sx: 'auto', md: '92vh'},
+            height: {sx: 'auto', md: '92vh'},
             borderRight: '1px solid #3d3d3d',
             px:{ sx: 0, md: 2}
         }} >
-
+          <Sidebar />
+          <Typography className="copyright" variant="body2" sx={{
+            mt: 2,
+            color: '#fff'
+          }}> 
+            &copy;Copyright 2023
+          </Typography>
         </Box>
      </Stack>
   )
