ea2e62a6b10cb46a09f6281126a4a871f36dd913
Shubhamx25 <shubhamy725p@gmail.com>
2023-06-20 18:25:56 +0530

    EOD 20 june

--- src/components/ChannelCard.js
+++ src/components/ChannelCard.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 import { demoProfilePicture } from "../utils/constants";
 
-const ChannelCard = ({ channelDetail}) => {
+const ChannelCard = ({ channelDetail, marginTop}) => {
   return (
     <Box
         sx={{
@@ -15,10 +15,10 @@ const ChannelCard = ({ channelDetail}) => {
            alignItems: 'center',
            width: { sx: '356px', md: '320px'},
            height: '326px',
-           margin:'auto'
-
+           margin:'auto',
+           marginTop
         }}>
-       <Link /* to={`/channel/${channelDetail?.id?.channelId}`} */ href='https://www.google.com'>
+       <Link to={`/channel/${channelDetail?.id?.channelId}`}>
           <CardContent sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', textAlign:'center', color: '#fff'}}>
             <CardMedia 
              image={channelDetail?.snippet?.thumbnails?.high?.url || demoProfilePicture} 
--- src/components/ChannelDetail.js
+++ src/components/ChannelDetail.js
@@ -1,8 +1,36 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Box } from "@mui/material";
+
+import { Videos, ChannelCard } from './';
+import { fetchFromApi } from "../utils/fetchFromApi";
 
 const ChannelDetail = () => {
+  const { id } = useParams();
+  const [channelDetail, setChannelDetail] = useState(null);
+  const [videos, setVideos] = useState([])
+  
+
+  useEffect(() => {
+      fetchFromApi(`channels?part="snippet&id=${id}`)
+       .then((data) => setChannelDetail(data?.items[0]));
+
+      fetchFromApi(`search?channelId=${id}&part=snippet&order=date`)
+       .then((data) => setVideos(data?.items));
+  },[id])
+
   return (
-    <div>ChannelDetail</div>
+    <Box minHeight='95vh'>
+      <Box>
+        <div style={{
+          background: 'linear-gradient(90deg, rgba(6,1,82,1) 0%, rgba(0,0,145,1) 35%, rgba(195,0,255,1) 100%)',
+          zIndex: 10,
+          height: '300px'
+        }}/>
+
+        <ChannelCard channelDetail={channelDetail} marginTop='-93px' />
+      </Box>
+    </Box>
   )
 }
 
