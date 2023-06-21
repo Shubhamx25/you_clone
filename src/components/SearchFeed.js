fb8e9b05aceaa279100d71ca7dc50b90bf9a5b3e
Shubhamx25 <shubhamy725p@gmail.com>
2023-06-21 10:18:07 +0530

    Started working in search

--- src/components/SearchFeed.js
+++ src/components/SearchFeed.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import { useState, useEffect } from "react"
+import { Box, Typography } from "@mui/material";
 
-const SearchFeed = () => {
+import { Videos } from "./index";
+import { fetchFromApi } from "../utils/fetchFromApi";
+
+const SearchFeed  = () => {
+
+  const [selectedCategory, setSelectedCategory] = useState('New');
+  const [videos, setVideos ] = useState([]);
+  
+  useEffect(() => {
+    fetchFromApi(`search?part=snippet&q=${selectedCategory}`)
+    .then((data) => setVideos(data.items))
+  },[selectedCategory]);
+
   return (
-    <div>SearchFeed</div>
+    <Box p={2} sx={{overflowY: 'auto', height:'90vh', flex: 2}}>
+    <Typography variant="h4" fontWeight='bold' mb={2} sx={{color:'white'}}>
+      {selectedCategory}<span style={{color: 'white'}}> videos</span>
+    </Typography>
+
+    <Videos videos={videos} />
+  </Box>
   )
 }
+
 
 export default SearchFeed
